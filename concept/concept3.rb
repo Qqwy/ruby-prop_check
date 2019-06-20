6479becb305ab451a514f455faaa93e5831ae1f7
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-06-21 01:27:17 +0200

    a lot more documentation

--- concept/concept3.rb
+++ concept/concept3.rb
@@ -167,21 +167,32 @@ end
 
 
 ##
-#
+# Contains common generators.
+# Use this module by including it in the class (e.g. in your test suite)
+# where you want to use them.
 module Generators
-
+  ##
+  # Always returns the same value, regardless of `size` or `rng` (random number generator state)
+  #
+  # >> Generators.constant(10)
   def constant(val)
     Generator.wrap(val)
   end
 
   private def integer_shrink(val)
+    # 0 cannot shrink further
     return [] if val.zero?
 
-    halvings = Helper.scanl(val) { |x| (x / 2.0).truncate }
-                 .take_while { |x| !x.zero? }
-                 .map { |x| val - x }
-                 .map { |x| LazyTree.new(x) }
+    # Numbers are shrunken by
+    # subtracting themselves, their half, quarter, eight, ... (rounded towards zero!) from themselves, until the number itself is reached.
+    # So: for 20 we have [0, 10, 15, 18, 19, 20]
+    halvings =
+      Helper.scanl(val) { |x| (x / 2.0).truncate }
+            .take_while { |x| !x.zero? }
+            .map { |x| val - x }
+            .map { |x| LazyTree.new(x) }
 
+    # For negative numbers, we also attempt if the positive number has the same result.
     if val.abs > val
       [LazyTree.new(val.abs, halvings)].lazy
     else
@@ -189,6 +200,18 @@ module Generators
     end
   end
 
+  ##
+  # Returns a random integer in the given range (if a range is given)
+  # or between 0..num (if a single integer is given).
+  #
+  # Does not scale when `size` changes.
+  # This means `choose` is useful for e.g. picking an element out of multiple possibilities,
+  # but for other purposes you probably want to use `integer` et co.
+  #
+  # >> r = Random.new(42); 10.times.map { Generators.choose(0..5).call(10, r) }
+  # => [3, 4, 2, 4, 4, 1, 2, 2, 2, 4]
+  # >> r = Random.new(42); 10.times.map { Generators.choose(0..5).call(20000, r) }
+  # => [3, 4, 2, 4, 4, 1, 2, 2, 2, 4]
   def choose(range)
     Generator.new do |_size, rng|
       val = rng.rand(range)
@@ -196,6 +219,17 @@ module Generators
     end
   end
 
+  ##
+  # A random integer which scales with `size`.
+  # Integers start small (around 0)
+  # and become more extreme (both higher and lower, negative) when `size` increases.
+  #
+  # >> Generators.integer.call(2, Random.new(42))
+  # => 2
+  # >> Generators.integer.call(10000, Random.new(42))
+  # => 5795
+  # >> r = Random.new(42); 10.times.map { Generators.integer.call(20000, r) }
+  # => [-4205, -19140, 18158, -8716, -13735, -3150, 17194, 1962, -3977, -18315]
   def integer
     Generator.new do |size, rng|
       val = rng.rand(-size..size)
@@ -203,14 +237,43 @@ module Generators
     end
   end
 
+  ##
+  # Only returns integers that are zero or larger.
+  # See `integer` for more information.
   def nonnegative_integer
-    integer.map { |x| x.abs }
+    integer.map(&:abs)
+  end
+
+  ##
+  # Only returns integers that are larger than zero.
+  # See `integer` for more information.
+  def positive_integer
+    nonnegative_integer.map { |x| x + 1 }
+  end
+
+  ##
+  # Only returns integers that are zero or smaller.
+  # See `integer` for more information.
+  def nonpositive_integer
+    nonnegative_integer.map(&:-@)
+  end
+
+  ##
+  # Only returns integers that are smaller than zero.
+  # See `integer` for more information.
+  def negative_integer
+    positive_integer.map(&:-@)
   end
 
   private def fraction(a, b, c)
-    a.to_f + b.to_f / ((c.to_f.abs) + 1.0)
+    a.to_f + b.to_f / (c.to_f.abs + 1.0)
   end
 
+  ##
+  # Generates floating point numbers
+  # These start small (around 0)
+  # and become more extreme (large positive and large negative numbers)
+  # TODO testing for NaN, Infinity?
   def float
     integer.bind do |a|
       integer.bind do |b|
@@ -221,12 +284,32 @@ module Generators
     end
   end
 
+  ##
+  # Generates a single-character string
+  # from the readable ASCII character set.
+  #
+  # >> r = Random.new(42); 10.times.map{choose(32..128).map(&:chr).call(10, r) }
+  # => ["S", "|", ".", "g", "\\", "4", "r", "v", "j", "j"]
+  def readable_ascii_char
+    choose(32..128).map(&:chr)
+  end
+
+  ##
+  # Picks one of the given `choices` at random uniformly every time.
   def one_of(*choices)
     choose(choices.length).bind do |index|
       choices[index]
     end
   end
 
+  ##
+  # Picks one of the choices given in `frequencies` at random every time.
+  # `frequencies` expects keys to be numbers
+  # (representing the relative frequency of this generator)
+  # and values to be generators.
+  #
+  # >> r = Random.new(42); 10.times.map { Generators.frequency(5 => Generators.integer, 1 => Generators.char).call(10, r) }
+  # => [4, -3, 10, 8, 0, -7, 10, 1, "E", 10]
   def frequency(frequencies)
     choices = frequencies.reduce([]) do |acc, elem|
       freq, val = elem
@@ -243,14 +326,6 @@ module Generators
         acc.map { |x| x << val }
       end
     end
-    # args.reduce(Generator.wrap([])) do |acc, generator|
-    #   acc.map
-    # end
-    # args.reduce([]) do |acc, generator|
-    #   generator.bind do |arg|
-    #     Generator.wrap(acc << arg)
-    #   end
-    # end
   end
 end
 
