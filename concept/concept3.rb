a18c51e45350ddf4f42a54d4537edf921b12e848
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-06-21 02:00:10 +0200

    Wrap property-related functions in module

--- concept/concept3.rb
+++ concept/concept3.rb
@@ -188,9 +188,9 @@ module Generators
     # So: for 20 we have [0, 10, 15, 18, 19, 20]
     halvings =
       Helper.scanl(val) { |x| (x / 2.0).truncate }
-            .take_while { |x| !x.zero? }
-            .map { |x| val - x }
-            .map { |x| LazyTree.new(x) }
+        .take_while { |x| !x.zero? }
+        .map { |x| val - x }
+        .map { |x| LazyTree.new(x) }
 
     # For negative numbers, we also attempt if the positive number has the same result.
     if val.abs > val
@@ -375,91 +375,94 @@ class PropertyCheckEvaluator
   end
 end
 
+module Property
 
-def forall(**bindings, &block)
+  def forall(**bindings, &block)
 
-  # Turns a hash of generators
-  # into a generator of hashes :D
-  binding_generator = tuple(*bindings.map { |key, generator| generator.map { |val| [key, val] } }).map { |val| val.to_h }
+    # Turns a hash of generators
+    # into a generator of hashes :D
+    binding_generator = tuple(*bindings.map { |key, generator| generator.map { |val| [key, val] } }).map { |val| val.to_h }
 
-  rng = Random::DEFAULT
-  n_successful = 0
-  generator_results = nil
-  begin
-    (1..1000).each do |size|
-      generator_results = binding_generator.generate(size, rng)
-      PropertyCheckEvaluator.new(generator_results.root, &block).call()
-      n_successful += 1
-    end
-  rescue Exception => problem
-    output = StringIO.new
-    output.puts ""
-    output.puts "FAILURE after #{n_successful} successful test runs. Failed on:"
-    output.puts "`#{print_roots(generator_results.root)}`"
-    output.puts ""
-    output.puts "Exception message: #{problem}"
-    output.puts ""
-    output.puts "Shrinking"
-    shrunken_result, shrunken_exception, num_shrink_steps = shrink(generator_results, output, &block)
-    output.puts ''
-    output.puts "Shrunken input after #{num_shrink_steps} steps:"
-    output.puts "`#{print_roots(shrunken_result)}`"
-    output.puts ""
-    output.puts "Shrunken exception: #{shrunken_exception}"
-    output.puts ""
-
-    raise problem, output.string, problem.backtrace
-  end
-end
-
-def print_roots(lazy_tree_hash)
-  lazy_tree_hash.map do |name, val|
-    "#{name} = #{val.inspect}"
-  end.join(", ")
-end
-
-def shrink(bindings_tree, output, &block)
-  problem_child = bindings_tree
-  problem_exception = nil
-  num_shrink_steps = 0
-  loop do
-    next_problem_child, next_problem_exception, child_shrink_steps = shrink_step(problem_child, output, &block)
-
-    break if next_problem_child.nil?
-
-    problem_child = next_problem_child
-    problem_exception = next_problem_exception
-    num_shrink_steps += child_shrink_steps
-  end
-
-  [problem_child.root, problem_exception, num_shrink_steps]
-end
-
-def shrink_step(bindings_tree, output, &block)
-  shrink_steps = 0
-  bindings_tree.children.each do |child|
-    shrink_steps += 1
-    output.print '.'
+    rng = Random::DEFAULT
+    n_successful = 0
+    generator_results = nil
     begin
-      PropertyCheckEvaluator.new(child.root, &block).call
+      (1..1000).each do |size|
+        generator_results = binding_generator.generate(size, rng)
+        PropertyCheckEvaluator.new(generator_results.root, &block).call()
+        n_successful += 1
+      end
     rescue Exception => problem
-      return [child, problem, shrink_steps]
+      output = StringIO.new
+      output.puts ""
+      output.puts "FAILURE after #{n_successful} successful test runs. Failed on:"
+      output.puts "`#{print_roots(generator_results.root)}`"
+      output.puts ""
+      output.puts "Exception message: #{problem}"
+      output.puts ""
+      output.puts "Shrinking"
+      shrunken_result, shrunken_exception, num_shrink_steps = shrink(generator_results, output, &block)
+      output.puts ''
+      output.puts "Shrunken input after #{num_shrink_steps} steps:"
+      output.puts "`#{print_roots(shrunken_result)}`"
+      output.puts ""
+      output.puts "Shrunken exception: #{shrunken_exception}"
+      output.puts ""
+
+      raise problem, output.string, problem.backtrace
     end
   end
 
-  [nil, nil, shrink_steps]
+  def print_roots(lazy_tree_hash)
+    lazy_tree_hash.map do |name, val|
+      "#{name} = #{val.inspect}"
+    end.join(", ")
+  end
+
+  def shrink(bindings_tree, output, &block)
+    problem_child = bindings_tree
+    problem_exception = nil
+    num_shrink_steps = 0
+    loop do
+      next_problem_child, next_problem_exception, child_shrink_steps = shrink_step(problem_child, output, &block)
+
+      break if next_problem_child.nil?
+
+      problem_child = next_problem_child
+      problem_exception = next_problem_exception
+      num_shrink_steps += child_shrink_steps
+    end
+
+    [problem_child.root, problem_exception, num_shrink_steps]
+  end
+
+  def shrink_step(bindings_tree, output, &block)
+    shrink_steps = 0
+    bindings_tree.children.each do |child|
+      shrink_steps += 1
+      output.print '.'
+      begin
+        PropertyCheckEvaluator.new(child.root, &block).call
+      rescue Exception => problem
+        return [child, problem, shrink_steps]
+      end
+    end
+
+    [nil, nil, shrink_steps]
+  end
 end
 
 include Generators
+include Property
 
 forall x: integer, z: float, y: nonnegative_integer do
   # puts x
   # puts y
   # puts z
 
-  if y == 42
-    raise "Boom!"
-  end
+  # if y == 42
+  #   raise "Boom!"
+  # end
 
   x > y == z
 end
