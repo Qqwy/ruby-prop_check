6e2e57a6f2b5f931c9653e2df9c809415736abf9
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-06-25 00:07:33 +0200

    Slightly more tests

--- spec/prop_check_spec.rb
+++ spec/prop_check_spec.rb
@@ -18,6 +18,20 @@ RSpec.describe PropCheck do
         expect { |block| PropCheck.forall(x: PropCheck::Generators.integer, &block) }.to yield_control
       end
     end
+
+    describe "Property" do
+      describe "#with_settings" do
+        it "updates the settings" do
+          p = PropCheck.forall(x: PropCheck::Generators.integer)
+          expect(p.settings[:verbose]).to be false
+          expect(p.with_settings(verbose: true).settings[:verbose]).to be true
+        end
+        it "Runs the property test when called with a block" do
+          expect { |block| PropCheck.forall(x: PropCheck::Generators.integer).with_settings({}, &block) }.to yield_control
+        end
+      end
+    end
   end
+
 end
 
