d49a48db2f77d8d1fec2f373db72a331aa6c1065
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-07-04 11:26:25 +0200

    fix documentation location

--- lib/prop_check/helper/lazy_append.rb
+++ lib/prop_check/helper/lazy_append.rb
@@ -1,10 +1,10 @@
-##
-# A refinement for enumerators
-# to allow lazy appending of two (potentially lazy) enumerators:
-#   >> [1,2,3].lazy_append([4,5.6]).to_a
-#   => [1,2,3,4,5,6]
 module PropCheck
   module Helper
+    ##
+    # A refinement for enumerators
+    # to allow lazy appending of two (potentially lazy) enumerators:
+    #   >> [1,2,3].lazy_append([4,5.6]).to_a
+    #   => [1,2,3,4,5,6]
     module LazyAppend
       refine Enumerable do
         ##   >> [1,2,3].lazy_append([4,5.6]).to_a
